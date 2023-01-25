ad417c48a8f1e4da8e298ac65677ac904df9eb45
EvanPiro <dev.evanpiro@gmail.com>
2023-01-25 11:52:40 -0500

    add dev shell

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "SignDBOracle";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-darwin";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system}.default = pkgs.hello;
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [];
+
+      shellHook = ''
+        curl -L https://foundry.paradigm.xyz | bash
+        source ~/.bashrc
+        foundryup
+      '';
+    };
+
+    formatter.${system} = pkgs.alejandra;
+  };
+}
