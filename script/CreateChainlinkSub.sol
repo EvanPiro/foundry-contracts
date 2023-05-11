49526ffd9f806d0b498196b1ae50a874584d4d98
EvanPiro <dev.evanpiro@gmail.com>
2023-05-11 09:06:00 -0400

    add subscription script placeholder

---
+++ script/CreateChainlinkSub.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+
+contract CreateChainLinkSub is Script {
+    function run() external {
+
+    }
+}
